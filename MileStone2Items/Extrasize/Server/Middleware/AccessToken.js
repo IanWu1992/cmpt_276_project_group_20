43feb4c0704c8b32996c5266769ec63c93488047
a456531@gmail.com <a456531@gmail.com>
2023-11-24 12:26:32 -0800

    added code and instruction

---
+++ MileStone2Items/Extrasize/Server/Middleware/AccessToken.js
@@ -0,0 +1,39 @@
+require("dotenv").config(); // loading data from env file
+const axios = require("axios");
+
+const authLink = "https://www.strava.com/oauth/token";
+
+function getActivities(res) {
+  const activitiesLink = `https://www.strava.com/api/v3/athlete/activities`;
+  // fetch users activities via API
+  axios
+    .get(activitiesLink, {
+      params: { access_token: res.access_token },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function reAuthorize() {
+  axios
+    .post(authLink, {
+      client_id: process.env.STRAVA_CLIENT_ID,
+      client_secret: process.env.STRAVA_CLIENT_SECRET,
+      refresh_token: process.env.STRAVA_REFRESH_TOKEN,
+      grant_type: "refresh_token",
+    })
+    .then((res) => getActivities(res.data))
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Perform the re-authorization
+reAuthorize();
+
+// Export the function
+module.exports = { reAuthorize };
